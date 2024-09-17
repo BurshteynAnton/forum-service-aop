9f8240668522a890697b40c42eb4ff8eb5897d40
BurshteynAnton <burshteynmail@gmail.com>
2024-09-17 16:51:33 +0300

    Initial commit

---
+++ src/main/java/telran/java53/accounting/service/UserAccountServiceImpl.java
@@ -0,0 +1,101 @@
+package telran.java53.accounting.service;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+import telran.java53.accounting.dao.UserAccountRepository;
+import telran.java53.accounting.dto.RolesDto;
+import telran.java53.accounting.dto.UserDto;
+import telran.java53.accounting.dto.UserEditDto;
+import telran.java53.accounting.dto.UserRegisterDto;
+import telran.java53.accounting.dto.exceptions.UserExistsException;
+import telran.java53.accounting.dto.exceptions.UserNotFoundException;
+import telran.java53.accounting.model.Role;
+import telran.java53.accounting.model.UserAccount;
+
+@Service
+@RequiredArgsConstructor
+public class UserAccountServiceImpl implements UserAccountService, CommandLineRunner {
+	
+	final UserAccountRepository userAccountRepository;
+	final ModelMapper modelMapper;
+	final PasswordEncoder passwordEncoder;
+
+	@Override
+	public UserDto register(UserRegisterDto userRegisterDto) {
+		if (userAccountRepository.existsById(userRegisterDto.getLogin())) {
+			throw new UserExistsException();
+		}
+		UserAccount userAccount = modelMapper.map(userRegisterDto, UserAccount.class);
+		String password = passwordEncoder.encode(userRegisterDto.getPassword());
+		userAccount.setPassword(password);
+		userAccountRepository.save(userAccount);
+		return modelMapper.map(userAccount, UserDto.class);
+	}
+
+	@Override
+	public UserDto getUser(String login) {
+		UserAccount userAccount = userAccountRepository.findById(login).orElseThrow(UserNotFoundException::new);
+		return modelMapper.map(userAccount, UserDto.class);
+	}
+
+	@Override
+	public UserDto removeUser(String login) {
+		UserAccount userAccount = userAccountRepository.findById(login).orElseThrow(UserNotFoundException::new);
+		userAccountRepository.delete(userAccount);
+		return modelMapper.map(userAccount, UserDto.class);
+	}
+
+	@Override
+	public UserDto updateUser(String login, UserEditDto userEditDto) {
+		UserAccount userAccount = userAccountRepository.findById(login).orElseThrow(UserNotFoundException::new);
+		if (userEditDto.getFirstName() != null) {
+			userAccount.setFirstName(userEditDto.getFirstName());
+		}
+		if (userEditDto.getLastName() != null) {
+			userAccount.setLastName(userEditDto.getLastName());
+		}
+		userAccountRepository.save(userAccount);
+		return modelMapper.map(userAccount, UserDto.class);
+	}
+
+	@Override
+	public RolesDto changeRolesList(String login, String role, boolean isAddRole) {
+		UserAccount userAccount = userAccountRepository.findById(login).orElseThrow(UserNotFoundException::new);
+		boolean res;
+		if (isAddRole) {
+			res = userAccount.addRole(role);
+		} else {
+			res = userAccount.removeRole(role);
+		}
+		if(res) {
+			userAccountRepository.save(userAccount);
+		}
+		return modelMapper.map(userAccount, RolesDto.class);
+	}
+
+	@Override
+	public void changePassword(String login, String newPassword) {
+		UserAccount userAccount = userAccountRepository.findById(login).orElseThrow(UserNotFoundException::new);
+		String password = passwordEncoder.encode(newPassword);
+		userAccount.setPassword(password);
+		userAccountRepository.save(userAccount);
+
+	}
+	
+	@Override
+	public void run(String... args) throws Exception {
+		if (!userAccountRepository.existsById("admin")) {
+			String password = passwordEncoder.encode("admin");
+			UserAccount userAccount = new UserAccount("admin", "", "", password );
+			userAccount.addRole(Role.MODERATOR.name());
+			userAccount.addRole(Role.ADMINISTRATOR.name());
+			userAccountRepository.save(userAccount);
+		}
+
+	}
+
+}
